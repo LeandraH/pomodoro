1125f2332c0618aff10bc8bae70def64ec9e6dc5
LeandraCL <leandra.hahn@crosslend.com>
2019-08-19 10:55:45 +0200

    Flesh Out CancelButton

--- src/CancelButton.js
+++ src/CancelButton.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
-const CancelButton = () => (
-  <button>Cancel</button>
+const CancelButton = ({ resetTimer }) => (
+  <button onClick={resetTimer}>Cancel</button>
 );
 
 export default CancelButton;
