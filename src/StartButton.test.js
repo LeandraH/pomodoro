ff559eb975531a29b05296992e410177d8212d15
LeandraCL <leandra.hahn@crosslend.com>
2019-08-27 23:34:16 +0200

    Fix StartButton Tests

--- src/StartButton.test.js
+++ src/StartButton.test.js
@@ -3,9 +3,13 @@ import { render, unmountComponentAtNode } from 'react-dom';
 import StartButton from './StartButton';
 
 let div;
+let startTimer;
+let pauseTimer;
 beforeEach(() => {
   div = document.createElement('div');
   document.body.appendChild(div);
+  startTimer = jest.fn();
+  pauseTimer = jest.fn();
 });
 
 afterEach(() => {
@@ -14,22 +18,20 @@ afterEach(() => {
 });
 
 it('renders without crashing', () => {
-  render(<StartButton />, div);
+  render(<StartButton startTimer={startTimer} pauseTimer={pauseTimer} />, div);
 });
 
 it('shows the correct text when the timer is not running', () => {
-  render(<StartButton isTimerRunning={false} />, div);
+  render(<StartButton startTimer={startTimer} pauseTimer={pauseTimer} isTimerRunning={false} />, div);
   expect(div.textContent).toEqual('Start');
 });
 
 it('shows the correct text when the timer is not running', () => {
-  render(<StartButton isTimerRunning />, div);
+  render(<StartButton startTimer={startTimer} pauseTimer={pauseTimer} isTimerRunning />, div);
   expect(div.textContent).toEqual('Pause');
 });
 
 it('calls the correct funtion when clicked while the timer is not running', () => {
-  const startTimer = jest.fn();
-  const pauseTimer = jest.fn();
   render(<StartButton
     startTimer={startTimer}
     pauseTimer={pauseTimer}
@@ -41,8 +43,6 @@ it('calls the correct funtion when clicked while the timer is not running', () =
 });
 
 it('calls the correct funtion when clicked while the timer is running', () => {
-  const startTimer = jest.fn();
-  const pauseTimer = jest.fn();
   render(<StartButton startTimer={startTimer} pauseTimer={pauseTimer} isTimerRunning />, div);
   document.querySelector('.start-button').dispatchEvent(new MouseEvent('click', { bubbles: true }));
   expect(startTimer).not.toHaveBeenCalled();
