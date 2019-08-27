560fe8d77b723a408282ec56b942e07cfc50b82a
LeandraCL <leandra.hahn@crosslend.com>
2019-08-27 13:21:31 +0200

    Eslint: Default Prop via React

--- src/StartButton.jsx
+++ src/StartButton.jsx
@@ -1,14 +1,18 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const StartButton = ({ startTimer, pauseTimer, isTimerRunning = false }) => (
+const StartButton = ({ startTimer, pauseTimer, isTimerRunning }) => (
   <button className="start-button" onClick={isTimerRunning ? pauseTimer : startTimer}>{isTimerRunning ? 'Pause' : 'Start'}</button>
 );
 
 export default StartButton;
 
-StartButton.prototype = {
+StartButton.propTypes = {
   startTimer: PropTypes.func.isRequired,
   pauseTimer: PropTypes.func.isRequired,
   isTimerRunning: PropTypes.bool,
+};
+
+StartButton.defaultProps = {
+  isTimerRunning: false,
 };
