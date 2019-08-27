266b0694c85448749ec82f4ab9f68e5420d29580
LeandraCL <leandra.hahn@crosslend.com>
2019-08-27 13:58:44 +0200

    Eslint: Add PropTypes to CancelButton

--- src/CancelButton.js
+++ src/CancelButton.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 const CancelButton = ({ resetTimer }) => (
   <button className="cancel-button" onClick={resetTimer}>Cancel</button>
 );
 
 export default CancelButton;
+
+CancelButton.propTypes = {
+  resetTimer: PropTypes.func.isRequired,
+};
