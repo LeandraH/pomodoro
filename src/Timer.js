a157f9c2293b8130dfaff581c4f6e14906f5d18c
LeandraCL <leandra.hahn@crosslend.com>
2019-08-27 13:40:19 +0200

    Eslint: Add Parentheses for Easier Human Understanding

--- src/Timer.js
+++ src/Timer.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const padZeroes = num => num < 10 ? `0${num}` : num;
+const padZeroes = (num) => (num < 10 ? `0${num}` : num);
 
 const Timer = ({ time }) => (
   <div className="show-time">{ padZeroes((Math.floor(time / 60)) % 60) }:{ padZeroes(time % 60) }</div>
